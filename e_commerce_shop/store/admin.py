17cffe7d6778dea7771be2b7d4675b0006e17b15
nvmarzakov <nikolay.marzakov@gmail.com>
2023-07-29 22:18:13 +0300

    Add admin panel for Category and Products

--- e_commerce_shop/store/admin.py
+++ e_commerce_shop/store/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Category, Product
+
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'slug'
+    ]
+
+    prepopulated_field = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'brand',
+        'slug',
+        'price',
+        'in_stock',
+        'created_at',
+        'updated_at',
+    ]
+
+    list_filter = [
+        'in_stock',
+        'is_active',
+    ]
+
+    list_editable = [
+        'price',
+        'in_stock',
+    ]
+
+    prepopulated_field = {'slug': ('title',)}
