b4be9d9e9f424d184a2d0b92787cdb89ef3c95b3
nvmarzakov <nikolay.marzakov@gmail.com>
2023-07-29 18:57:23 +0300

    Set Database to PostgreSQL and add store app

--- e_commerce_shop/settings.py
+++ e_commerce_shop/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'e_commerce_shop.store',
 ]
 
 MIDDLEWARE = [
@@ -75,9 +78,13 @@ WSGI_APPLICATION = 'e_commerce_shop.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "e_commerce_db",
+        "USER": "marzaka",
+        "PASSWORD": "Kosmonaft1",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
