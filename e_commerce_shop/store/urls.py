62c4335e5fc8921a1d290b4c3eb101035a6a2f94
nvmarzakov <nikolay.marzakov@gmail.com>
2023-07-30 18:42:42 +0300

    add path to product_detail

--- e_commerce_shop/store/urls.py
+++ e_commerce_shop/store/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'store'
 
 urlpatterns = (
-    # path('', views.all_products, name='all_products'),
     path('', views.AllProductsListView.as_view(), name='all_products'),
-    path('item/<int:pk>', views.product_detail, name='product_detail')
+    path('item/<int:pk>', views.ProductDetailView.as_view(), name='product_detail'),
 )
