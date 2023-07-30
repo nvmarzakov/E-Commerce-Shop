3328991674a74e83b40ac9d0db12c606701fca67
nvmarzakov <nikolay.marzakov@gmail.com>
2023-07-30 11:41:16 +0300

    create and test urls and view at home.html

--- e_commerce_shop/store/urls.py
+++ e_commerce_shop/store/urls.py
@@ -1,6 +1,9 @@
 # store urls.py
 from django.urls import path
+from . import views
+
+app_name = 'store'
 
 urlpatterns = (
-
+    path('', views.all_products, name='all_products'),
 )
--- e_commerce_shop/urls.py
+++ e_commerce_shop/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('store', include('e_commerce_shop.store.urls')),
+    path('', include('e_commerce_shop.store.urls', namespace='store')),
 ]
 
 if settings.DEBUG:
