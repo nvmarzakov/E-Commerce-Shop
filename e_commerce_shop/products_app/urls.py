7e847034af39372d70538bbd14be207639c4be86
nvmarzakov <nikolay.marzakov@gmail.com>
2023-08-02 13:29:19 +0300

    added path to CategoryListView

--- e_commerce_shop/products_app/urls.py
+++ e_commerce_shop/products_app/urls.py
@@ -7,4 +7,5 @@ app_name = 'products_app'
 urlpatterns = (
     path('', views.AllProductsListView.as_view(), name='all_products'),
     path('item/<int:pk>', views.ProductDetailView.as_view(), name='product_detail'),
+    path('search/<slug:category_slug>/', views.CategoryListView.as_view(), name='category_list')
 )
