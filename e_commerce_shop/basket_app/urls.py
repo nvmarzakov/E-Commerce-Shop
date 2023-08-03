90bc12358c61fd314a7bcfb339dc2880c4f6c209
nvmarzakov <nikolay.marzakov@gmail.com>
2023-08-03 23:15:47 +0300

    added path basket_add

--- e_commerce_shop/basket_app/urls.py
+++ e_commerce_shop/basket_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from e_commerce_shop.basket_app.views import basket_summary
+from e_commerce_shop.basket_app.views import basket_summary, basket_add
 
 app_name = 'basket_app'
 
 urlpatterns = (
     path('', basket_summary, name='basket_summary'),
+    path('add/', basket_add, name='basket_add'),
 )
