e84c800e148e492a3a5d24f90de4e502d15cd004
nvmarzakov <nikolay.marzakov@gmail.com>
2023-08-06 22:22:10 +0300

    Get total price on the basket

--- e_commerce_shop/basket_app/basket.py
+++ e_commerce_shop/basket_app/basket.py
@@ -60,3 +60,5 @@ class Basket():
         """
         return sum(item['qty'] for item in self.basket.values())
 
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
