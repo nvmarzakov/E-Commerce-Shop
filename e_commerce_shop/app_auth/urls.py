126a35a9d5c35bd7cbe58fc8dce882f5eef7b218
nvmarzakov <nikolay.marzakov@gmail.com>
2023-08-13 05:54:26 +0300

    added paths : user details,edit, delete

--- e_commerce_shop/app_auth/urls.py
+++ e_commerce_shop/app_auth/urls.py
@@ -1,13 +1,17 @@
 # app_auth/urls.py
 from django.urls import path
 
-from e_commerce_shop.app_auth.views import RegisterUserView, LoginUserView, LogoutUserView, UsersListView
+from e_commerce_shop.app_auth.views import RegisterUserView, LoginUserView, LogoutUserView, UsersListView, \
+    UserProfileDetailView, UserProfileEditView, UserProfileDeleteView
 
 urlpatterns = (
     path('register/', RegisterUserView.as_view(), name='register_user'),
     path('login/', LoginUserView.as_view(), name='login_user'),
     path('logout/', LogoutUserView.as_view(), name='logout_user'),
-    path('', UsersListView.as_view(), name='users_list'),
+    path('profile-details/<int:pk>', UserProfileDetailView.as_view(), name='details_user'),
+    path('edit-profile/<int:pk>', UserProfileEditView.as_view(), name='edit_user'),
+    path('delete-profile/<int:pk>', UserProfileDeleteView.as_view(), name='delete_user'),
+    path('', UsersListView.as_view(), name='user_list'),
 )
 
 # Marzakov85
