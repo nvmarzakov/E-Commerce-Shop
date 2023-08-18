bd74d38119cd59e5e011dd73f8180ff5b4370126
nvmarzakov <nikolay.marzakov@gmail.com>
2023-08-18 14:28:04 +0300

    create ContactSubmission model

--- e_commerce_shop/common_app/models.py
+++ e_commerce_shop/common_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactSubmission(models.Model):
+    first_name = models.CharField(
+        max_length=100,
+    )
+
+    last_name = models.CharField(
+        max_length=100,
+    )
+
+    email = models.EmailField(
+        blank=False,
+        null=False,
+    )
+
+    subject = models.CharField(
+        max_length=200,
+    )
+
+    message = models.TextField(
+        blank=False,
+        null=False,
+    )
+
+    timestamp = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return f'{self.subject} by {self.first_name} {self.last_name}'
