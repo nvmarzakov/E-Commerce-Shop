fef90ba6092f425a08d4a7ed01a67b6578adca02
nvmarzakov <nikolay.marzakov@gmail.com>
2023-08-18 14:29:54 +0300

    added contact form

---
+++ e_commerce_shop/common_app/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import ContactSubmission
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = ContactSubmission
+        fields = '__all__'
+
+
